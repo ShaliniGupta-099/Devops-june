e80a41e7c0a18177fc91e4c6a42cfdab0ad345d0
ShaliniGupta-099 <shalinigupta.psit@gmail.com>
2026-06-05 00:53:15 +0530

    Day4 :First Bas Automation Script -DevOps Journeyh

---
+++ day4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Day4:DevOps Automation- my first bash script
+echo "===Day4:DevOps Automation Started==="
+echo "today's date:4 june 2026"
+echo " My name is Shalini and this is my first bash script"
+
+echo ""
+echo "---Server Folder Check---"
+pwd
+echo ""
+
+echo "---Files List---"
+ls -l
+echo ""
+
+echo "Status: Day4 Completed"
